bb1d6126be0728c6af7b8d5cc42245dcb1c6be69
ahangel03 <114983874+ahangel03@users.noreply.github.com>
2022-11-04 13:48:14 +0100

    Add files via upload

--- ipc5.c
+++ ipc5.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h> 
 
 void main(){
 
-     int fd[2]; 
+     int fd1[2]; 
+     int fd2[2]; 
      char buffer[30];
      pid_t pid;
      int dni;
@@ -18,20 +19,23 @@ void main(){
      
     
      // Creamos el pipe
-     pipe(fd); 
-     
+     pipe(fd1); 
+     pipe(fd2);
      //Se crea un proceso hijo
      pid = fork();
 
      if (pid==0)
      
-     {
-              
+     { 	close(fd1[0]); 
+               close(fd2[1]);
                 printf("Introduzca el numero de tu dni:\n");
-                close(fd[0]); // Cierra el descriptor de escritura
+               // Cierra el descriptor de escritura
+               
                 scanf("%d",&dni);
-                write(fd[1], &dni, 10);
-                   
+                sprintf(buffer,"%d",dni);
+                write(fd1[1], buffer, sizeof(buffer));
+                read(fd2[0],buffer,sizeof(buffer));
+                  printf("La letra del nif es :%d",letra[valor]);
      
      }
      
@@ -39,12 +43,16 @@ void main(){
      
      {
       
-                close(fd[1]); // Cierra el descriptor de lectura
-	        read(fd[0],buffer,sizeof(buffer));
-
-		valor=buffer;
+                close(fd1[1]);
+                close(fd2[0]); // Cierra el descriptor de lectura
+	        read(fd1[0],buffer,sizeof(buffer));//el padre lee el pipe 1
+	        printf("%s",buffer);
+		
+		valor=atoi(buffer);
 	       valor%=23;
 	        printf("la letra del NIF es:%c",letra[valor]);
+	        sprintf(buffer,"%d",dni);
+	        write(fd2[1],buffer,sizeof(buffer));
                	        wait(NULL);
                                  
 
